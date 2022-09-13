8e3e62c7799643e011899d4c7de2cbc1d9cb738d
Njols <niels@tonsoftime.com>
2022-09-13 15:50:27 +0200

    added navigation for the enemies using NavMesh Plus

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 public enum EnemyMovementState
 {
     Idle,
@@ -8,33 +10,16 @@ public enum EnemyMovementState
 }
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] private float _movementSpeed = 10f;
-    [SerializeField] private Transform _enemyRoot;
-    private Vector2 _positionToMoveTowards;
-    public EnemyMovementState State { get; private set; }
-    private void OnEnable()
+    [SerializeField] private NavMeshAgent _agent;
+    private void Start()
     {
-        State = EnemyMovementState.Idle;
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
     }
-    private void Update()
+    public void MoveTowards(Vector2 position)
     {
-        if (State == EnemyMovementState.MovingTowards)
-        {
-            var newPosition = Vector2.MoveTowards(_enemyRoot.position, _positionToMoveTowards, _movementSpeed * Time.deltaTime);
-            _enemyRoot.position = newPosition;
-        }
-    }
-    public void MoveTowards (Vector2 position)
-    {
-        if (position != null)
-        {
-            _positionToMoveTowards = position;
-            State = EnemyMovementState.MovingTowards;
-        }
-
-    }
-    public void StopMoving ()
-    {
-        State = EnemyMovementState.Idle;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(position, out hit, 100f, -1);
+        _agent.destination = hit.position;
     }
 }
