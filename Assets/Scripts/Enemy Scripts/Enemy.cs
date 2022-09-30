947adca4b31ad4899cf2501e853e9545e787dcc4
Njols <niels@tonsoftime.com>
2022-09-30 12:18:54 +0200

    changed it so that the enemy class doesn't need a list of possible states anymore, also added proof of concept melee and ranged enemy

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -6,16 +6,16 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public EnemyState ActiveState { get; private set; }
-    [SerializeField] private EnemyState[] possibleStates;
     public Transform enemyRoot;
     public Transform targetedPlayer;
     public EnemyMovement movement;
     [HideInInspector] public float meleeAttackCooldown;
     private float wtfTimer = 0f;
+    [SerializeField] private EnemyState _firstState;
     public Animator animator;
     private void OnEnable()
     {
-        ActiveState = FindStateByType(StateType.Idle);
+        ActiveState = _firstState;
         targetedPlayer = FindObjectOfType<PlayerAttribute>().transform;
     }
     private void Update()
@@ -26,20 +26,7 @@ public class Enemy : MonoBehaviour
     public void ChangeState(EnemyState preferredNewState)
     {
         wtfTimer++;
-        Debug.Log(preferredNewState.ToString() + " " + wtfTimer);
-        if (!possibleStates.Contains(preferredNewState)) throw new System.Exception("State " + preferredNewState.ToString() + " not found in possible states!");
         preferredNewState.DoStart();
         ActiveState = preferredNewState;
-    }
-    private EnemyState FindStateByType(StateType type)
-    {
-        List<EnemyState> foundStates = new List<EnemyState>();
-        foreach (EnemyState state in possibleStates)
-        {
-            if (state.Type == type) foundStates.Add(state);
-        }
-        if (foundStates.Count == 1) return foundStates[0];
-        else if (foundStates.Count == 0) throw new System.Exception("No state of this type!");
-        else return foundStates[0];
     }
 }
