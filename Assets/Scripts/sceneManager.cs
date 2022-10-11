bdea2865318a22c009cd1da98dce1f0b8ae8fa21
marinastancu <448358@student.fontys.nl>
2022-10-11 15:51:35 +0200

    Fixed restart button

--- Assets/Scripts/sceneManager.cs
+++ Assets/Scripts/sceneManager.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class sceneManager : MonoBehaviour
 {
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("clicked!");
+    }
+
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
