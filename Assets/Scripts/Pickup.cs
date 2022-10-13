192a7f7f2bc3ff0f08bad0425e4c8885e8240be7
marinastancu <448358@student.fontys.nl>
2022-10-14 01:35:14 +0200

    Finished functionality for weapon pickup with new melee weapon; weapon animation needs polish, pickup needs bugfix

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -2,36 +2,17 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    [Header("Input")]
-    public PlayerMovement input;
-
-    PlayerInventory inventory;
-    GameObject weaponToDrop;
     GameObject player;
     bool performingPickupAction=false;
-
-    //Setting attributes, sprites, animations
-    WeaponInformation weaponInformation;
-    SpriteRenderer spriteRenderer;
-    SpriteRenderer attackSprite;
-    PlayerAttribute playerAttribute;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        input = player.GetComponent<PlayerMovement>();
-        inventory = player.GetComponent<PlayerInventory>();
-
-        //Setting attributes, sprites, animations
-        spriteRenderer=gameObject.GetComponent<SpriteRenderer>();
-        attackSprite = player.GetComponentsInChildren<SpriteRenderer>()[1];
-        weaponInformation = gameObject.GetComponent<WeaponInformation>();
-        playerAttribute=player.GetComponent<PlayerAttribute>();
     }
 
     private void Update()
     {
-        if (input.controls.Player.Interact.IsPressed())
+        if (player.GetComponent<PlayerMovement>().controls.Player.Interact.IsPressed())
         {
             performingPickupAction = true;
         }
@@ -43,40 +24,10 @@ public class Pickup : MonoBehaviour
         {
             if (performingPickupAction)
             {
-                if (!weaponInformation.isRangedWeaponType)
-                {
-                    weaponToDrop = inventory.meleeWeapon;
-                    inventory.meleeWeapon = gameObject;
-
-                    //Setting attributes, sprites, animations
-                    attackSprite.sprite = spriteRenderer.sprite;
-                    playerAttribute.AttackInfoArray = weaponInformation.attackInfo;
-                    StateMachine.Instance.weaponName = weaponInformation.animationName;
-                }
-
-                else
-                {
-                    weaponToDrop = inventory.rangedWeapon;
-                    inventory.rangedWeapon = gameObject;
-
-                    //Setting attributes, sprites, animations
-                    attackSprite.sprite = spriteRenderer.sprite;
-                    playerAttribute.AttackInfoArray = weaponInformation.attackInfo;
-                    StateMachine.Instance.weaponName = weaponInformation.animationName;
-                }
-                //Move the new weapon from the floor to the player's hand
-                gameObject.transform.parent = player.transform;
-                gameObject.GetComponent<Collider2D>().enabled = false;
+                player.GetComponent<PlayerInventory>().SwitchWeapon(gameObject, gameObject.GetComponent<WeaponInformation>().isRangedWeaponType);
                 gameObject.SetActive(false);
-
-                // Drop the previous weapon from the player's hand to the floor (only if the player has had previous weapons)
-                if (weaponToDrop != null)
-                {
-                    weaponToDrop.transform.parent = null;
-                    weaponToDrop.GetComponent<Collider2D>().enabled = true;
-                }
-                performingPickupAction = false;
             }
+            performingPickupAction = false;
         }
     }
 }
--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -8,4 +8,72 @@ public class PlayerInventory : MonoBehaviour
     [SerializeField]
     public GameObject rangedWeapon;
     public GameObject meleeWeapon;
+    public GameObject weaponToDrop;
+
+    GameObject player;
+
+    public void Start()
+    {
+        player = gameObject;
+        //Set meleeWeapon to the default weapon in the player's hands
+        meleeWeapon = new GameObject();
+        meleeWeapon.name = "meleeWeapon";
+        meleeWeapon.AddComponent<SpriteRenderer>();
+        meleeWeapon.AddComponent<BoxCollider2D>();
+        meleeWeapon.GetComponent<BoxCollider2D>().size = new Vector2(0.16f, 0.16f);
+        meleeWeapon.transform.localScale = new Vector3(6f, 6f, 0f);
+        meleeWeapon.GetComponent<BoxCollider2D>().isTrigger = true;
+        meleeWeapon.AddComponent<Pickup>();
+        meleeWeapon.AddComponent<WeaponInformation>();
+        meleeWeapon.GetComponent<Collider2D>().enabled = false;
+        meleeWeapon.SetActive(false);
+        SetDefaultAttributes(meleeWeapon);
+    }
+
+    public void SwitchWeapon(GameObject newWeapon, bool isRangedWeapon)
+    {
+        if (isRangedWeapon)
+        {
+            weaponToDrop = rangedWeapon;
+            rangedWeapon = newWeapon;
+            SetAttributesOnWeapon(newWeapon);
+            DropPreviousWeapon(weaponToDrop);
+        }
+
+        else
+        {
+            weaponToDrop = meleeWeapon;
+            meleeWeapon = newWeapon;
+            SetAttributesOnWeapon(newWeapon);
+            DropPreviousWeapon(weaponToDrop);
+        }
+    }
+
+    public void DropPreviousWeapon(GameObject weaponToDrop)
+    {
+        //Drop the previous weapon from the player's hand to the floor (only if the player has had previous weapons)
+        if (weaponToDrop != null)
+        {
+            weaponToDrop.transform.position = player.transform.position;
+            weaponToDrop.transform.parent = null;
+            weaponToDrop.SetActive(true);
+            weaponToDrop.GetComponent<Collider2D>().enabled = true;
+        }
+    }
+
+    public void SetAttributesOnWeapon(GameObject weapon)
+    {
+        //Setting attributes, sprites, animations
+        player.GetComponentsInChildren<SpriteRenderer>()[1].sprite = weapon.GetComponent<SpriteRenderer>().sprite;
+        player.GetComponent<PlayerAttribute>().AttackInfoArray = weapon.GetComponent<WeaponInformation>().attackInfo;
+        StateMachine.Instance.weaponName = weapon.GetComponent<WeaponInformation>().animationName;
+    }
+
+    public void SetDefaultAttributes(GameObject weapon)
+    {
+        //Setting attributes, sprites, animations
+        weapon.GetComponent<WeaponInformation>().sprite = player.GetComponentsInChildren<SpriteRenderer>()[1].sprite;
+        weapon.GetComponent<WeaponInformation>().attackInfo = player.GetComponent<PlayerAttribute>().AttackInfoArray;
+        weapon.GetComponent<WeaponInformation>().animationName = StateMachine.Instance.weaponName;
+    }
 }
